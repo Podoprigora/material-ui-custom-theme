c3e65bcb7425bf62fc77e27fd3304e37b7d0b335
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2021-07-19 12:53:31 +0300

    Refactoring

--- src/stories/Popper/Popper.stories.tsx
+++ src/stories/Popper/Popper.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Popper, PopperProps, Paper, Fade, IconButton, Icon } from '@material-ui/core';
@@ -11,10 +11,10 @@ export default {
     component: Popper
 } as Meta;
 
-export const Default: Story<PopperProps> = () => {
+export const Example: Story<PopperProps> = () => {
     const [anchorRef, setAnchorRef] = useState<HTMLButtonElement | null>(null);
     const [arrowRef, setArrowRef] = useState<HTMLDivElement | null>(null);
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
 
     const handleButtonClick = useCallback(() => {
         setOpen((prevState) => !prevState);
@@ -24,30 +24,27 @@ export const Default: Story<PopperProps> = () => {
         setOpen(false);
     }, []);
 
-    const popperProps = useMemo<PopperProps>(
-        () => ({
-            open,
-            anchorEl: anchorRef,
-            placement: 'top',
-            transition: true,
-            modifiers: [
-                {
-                    name: 'offset',
-                    options: {
-                        offset: [0, 20]
-                    }
-                },
-                {
-                    name: 'arrow',
-                    enabled: true,
-                    options: {
-                        element: arrowRef
-                    }
+    const popperProps = {
+        open,
+        anchorEl: anchorRef,
+        placement: 'top',
+        transition: true,
+        modifiers: [
+            {
+                name: 'offset',
+                options: {
+                    offset: [0, 20]
                 }
-            ]
-        }),
-        [open, anchorRef, arrowRef]
-    );
+            },
+            {
+                name: 'arrow',
+                enabled: true,
+                options: {
+                    element: arrowRef
+                }
+            }
+        ]
+    } as PopperProps;
 
     return (
         <div style={{ margin: '20rem' }}>
@@ -62,34 +59,36 @@ export const Default: Story<PopperProps> = () => {
                     <MapPinSvg />
                 </Icon>
             </IconButton>
-            <Popper {...popperProps} className="popper map-popper">
-                {({ TransitionProps }) => {
-                    return (
-                        <Fade {...TransitionProps}>
-                            <Paper>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo,
-                                aperiam sapiente cumque delectus esse sunt possimus, labore commodi,
-                                repudiandae quam eaque iste atque soluta? Voluptas quisquam harum
-                                quaerat facere quos!
-                                <div
-                                    ref={setArrowRef}
-                                    data-popper-arrow
-                                    className="popper__arrow"
-                                />
-                                <IconButton
-                                    size="small"
-                                    className="map-popper__close MuiIconButton-dense"
-                                    onClick={handleClose}
-                                >
-                                    <Icon>
-                                        <XSvg />
-                                    </Icon>
-                                </IconButton>
-                            </Paper>
-                        </Fade>
-                    );
-                }}
-            </Popper>
+            {popperProps.anchorEl && (
+                <Popper {...popperProps} className="popper map-popper">
+                    {({ TransitionProps }) => {
+                        return (
+                            <Fade {...TransitionProps}>
+                                <Paper>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo,
+                                    aperiam sapiente cumque delectus esse sunt possimus, labore
+                                    commodi, repudiandae quam eaque iste atque soluta? Voluptas
+                                    quisquam harum quaerat facere quos!
+                                    <div
+                                        ref={setArrowRef}
+                                        data-popper-arrow
+                                        className="popper__arrow"
+                                    />
+                                    <IconButton
+                                        size="small"
+                                        className="map-popper__close MuiIconButton-dense"
+                                        onClick={handleClose}
+                                    >
+                                        <Icon>
+                                            <XSvg />
+                                        </Icon>
+                                    </IconButton>
+                                </Paper>
+                            </Fade>
+                        );
+                    }}
+                </Popper>
+            )}
         </div>
     );
 };
