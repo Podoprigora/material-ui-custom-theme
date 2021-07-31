0237614f810dda9f1e591ed28b8ba05364098ce4
Alexandr Podoprigora <podoprigoraisv@gmail.com>
2021-08-01 02:47:54 +0300

    Add FieldControl and InputBase styles

--- src/stories/Input/Input.stories.tsx
+++ src/stories/Input/Input.stories.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Meta, Story } from '@storybook/react/types-6-0';
 import {
-    InputBase,
-    FormControl,
-    FormLabel,
-    InputAdornment,
     Button,
+    FormControl,
+    FormHelperText,
+    FormLabel,
     Icon,
-    IconButton
+    IconButton,
+    InputAdornment,
+    InputBase,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText
 } from '@material-ui/core';
-import { AlternateEmailRounded, MailOutlineRounded, SearchRounded } from '@material-ui/icons';
+import { InsertDriveFileOutlined } from '@material-ui/icons';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
 import {
     EyeOffSvg,
-    EyeSvg,
+    InboxSvg,
     LockSvg,
     MapPinSvg,
     SaveSvg,
@@ -27,7 +32,10 @@ export default {
 export const InputBaseExample: Story = () => {
     return (
         <>
-            <div className="stack stack--direction-column stack--gap-10">
+            <div
+                className="stack stack--direction-column stack--gap-10"
+                style={{ width: '100%', maxWidth: '40rem' }}
+            >
                 <Button
                     size="medium"
                     variant="contained"
@@ -39,25 +47,31 @@ export const InputBaseExample: Story = () => {
                 >
                     Save changes
                 </Button>
-                <div className="stack stack--alignItemsEnd stack--justifyItemsStart stack--justifySelfStretch">
-                    <FormControl required sx={{ width: '30rem' }}>
-                        <FormLabel>Email</FormLabel>
-                        <InputBase
-                            type="email"
-                            placeholder="Enter your email"
-                            startAdornment={
-                                <InputAdornment position="start">
-                                    <Icon>
-                                        <UserSvg />
-                                    </Icon>
-                                </InputAdornment>
-                            }
-                        />
-                    </FormControl>
-                    <Button variant="contained">Submit</Button>
-                </div>
-                <FormControl required sx={{ width: '30rem' }}>
+                <FormControl
+                    variant="outlined"
+                    required
+                    className="MuiFormControl-labelAlignTop"
+                    fullWidth
+                >
+                    <FormLabel htmlFor="email-input">Email</FormLabel>
                     <InputBase
+                        id="email-input"
+                        type="email"
+                        placeholder="Enter your email"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <Icon>
+                                    <UserSvg />
+                                </Icon>
+                            </InputAdornment>
+                        }
+                    />
+                    <FormHelperText>example@gmail.com</FormHelperText>
+                </FormControl>
+                <FormControl required fullWidth className="MuiFormControl-labelAlignTop">
+                    <FormLabel htmlFor="password-input">Password</FormLabel>
+                    <InputBase
+                        id="password-input"
                         type="password"
                         placeholder="Enter password"
                         startAdornment={
@@ -72,6 +86,9 @@ export const InputBaseExample: Story = () => {
                                 <IconButton
                                     size="small"
                                     className="MuiIconButton-dense MuiIconButton-circular"
+                                    onMouseDown={(ev: React.MouseEvent) => {
+                                        ev.preventDefault();
+                                    }}
                                 >
                                     <Icon>
                                         <EyeOffSvg />
@@ -81,8 +98,10 @@ export const InputBaseExample: Story = () => {
                         }
                     />
                 </FormControl>
-                <FormControl required sx={{ width: '30rem' }}>
+                <FormControl color="secondary" className="MuiFormControl-labelAlignTop" fullWidth>
+                    <FormLabel htmlFor="placement-input">Location</FormLabel>
                     <InputBase
+                        id="placement-input"
                         type="text"
                         placeholder="Enter coordinates"
                         endAdornment={
@@ -93,9 +112,16 @@ export const InputBaseExample: Story = () => {
                             </InputAdornment>
                         }
                     />
+                    <FormHelperText>Example: 51.5091996,-0.1209507</FormHelperText>
                 </FormControl>
-                <FormControl sx={{ width: '30rem' }}>
-                    <InputBase placeholder="Enter your comment" multiline minRows={4} />
+                <FormControl color="secondary" fullWidth className="MuiFormControl-labelAlignTop">
+                    <FormLabel htmlFor="comment-input">Comment</FormLabel>
+                    <InputBase
+                        id="comment-input"
+                        placeholder="Enter your comment"
+                        multiline
+                        minRows={4}
+                    />
                 </FormControl>
             </div>
         </>
